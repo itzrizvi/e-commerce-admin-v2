bb7f5072173f5662e63783606156f1ac2b01a3de
foysalBN <foysalbinnour@gmail.com>
2022-10-02 03:16:27 +0600

    basic product page added

--- src/routes/admin/products.js
+++ src/routes/admin/products.js
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Dashboard = lazy(() => import('../../container/dashboard'));
+const AddProduct = lazy(() => import('../../container/products/AddProduct'));
 
 const ProductRoutes = () => {
   const { path } = useRouteMatch();
   return (
     <Switch>
       <Route exact path={path} component={Dashboard} />
-      <Route path={`${path}/add`} component={Dashboard} />
+      <Route path={`${path}/add`} component={AddProduct} />
     </Switch>
   );
 };
