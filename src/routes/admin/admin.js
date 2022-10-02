da09d2b9615919b5e77791b95c761ac2be1d4d0c
foysalBN <foysalbinnour@gmail.com>
2022-10-02 21:08:16 +0600

    minor changes

--- src/routes/admin/admin.js
+++ src/routes/admin/admin.js
@@ -2,14 +2,14 @@ import React, { lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
 
-const Dashboard = lazy(() => import('../../container/dashboard'));
+const AddAdmin = lazy(() => import('../../container/setings/AddAdmin'));
 
 const AdminRoutes = () => {
   const { path } = useRouteMatch();
   return (
     <Switch>
-      <Route exact path={path} component={Dashboard} />
-      <Route path={`${path}/add`} component={Dashboard} />
+      {/* <Route exact path={path} component={AddAdmin} /> */}
+      <Route path={`${path}/add`} component={AddAdmin} />
     </Switch>
   );
 };
