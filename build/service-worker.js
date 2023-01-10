a366732bc23cc288fb4ab10f6bf2c25d7dab7ff9
itzrizvi <shahriar.me.work@gmail.com>
2023-01-10 20:27:21 +0600

    Customer Add UI Revised

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3178171f034597e1c10ca8e72d7cf9b5.js"
+  "/precache-manifest.b089625dd8e3f36336a318c30a34d315.js"
 );
 
 self.addEventListener('message', (event) => {
